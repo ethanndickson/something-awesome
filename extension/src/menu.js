50ed26064de6b92213e867b05ff8cc322726429a
ethanndickson <ethanndickson@gmail.com>
2022-03-28 18:33:14 +1100

    button toggle implemented w/ changing colours

--- extension/src/menu.js
+++ extension/src/menu.js
@@ -1,11 +1,16 @@
 const mainbutton = document.getElementById("mainbutton");
-// ge & set starting colour 
+
+// get & set starting colour 
+chrome.storage.local.get(['enabled'],function(result){
+    if (result.enabled) { mainbutton.style.borderColor = 'lightgreen' } // green
+    else { mainbutton.style.borderColor = 'red' }
+});
 
 // toggle enabled bool in storage 
 mainbutton.addEventListener("click", (ev) => {
     chrome.storage.local.get(['enabled'],function(result){
         chrome.storage.local.set({ enabled: !(result.enabled) });
-        if (result.enabled) { mainbutton.style.borderColor = 'lightgreen' } // green
+        if (!result.enabled) { mainbutton.style.borderColor = 'lightgreen' } // green
         else { mainbutton.style.borderColor = 'red' }
     });
 });
