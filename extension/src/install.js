78ca51c59298161875aa75115ecddf03053bd34a
ethanndickson <ethanndickson@gmail.com>
2022-04-07 16:32:38 +1000

    fixed a bug with not getting client id

--- extension/src/install.js
+++ extension/src/install.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
     /* Setup persistence */
     chrome.storage.local.set({ enabled: true});
-    fetch('http://127.0.0.1:3000/create').then(res => res.json()).then((data) => {  chrome.storage.local.set({ id: data.id }) });
+    fetch('http://127.0.0.1:3000/create').then(res => res.json()).then((data) => {  chrome.storage.local.set({ id: data });});
 });
